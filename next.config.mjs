9cdd2276d8431e490ce7d903ef50cbf5fd78076c
Morganocode <103151864+Morganocode@users.noreply.github.com>
2025-06-23 16:35:47 +0200

    maj

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = 'Website-OpenMC'; // Mets ici le nom de ton repo
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: isGithubActions ? `/${repo}` : '',
+    assetPrefix: isGithubActions ? `/${repo}/` : '',
     images: {
+        unoptimized: true, // Important pour GitHub Pages
         domains: ['avatars.githubusercontent.com', 'crafatar.com'],
     },
 }
