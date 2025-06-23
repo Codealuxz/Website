0952bd2e8c5014fa38c4a4e9ec13129d04af9a89
Morganocode <103151864+Morganocode@users.noreply.github.com>
2025-06-23 20:03:08 +0200

    teste 36 000

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['avatars.githubusercontent.com', 'crafatar.com'],
     },
+    output: 'export',
+    // basePath: '/Website', // décommente si tu déploies sur https://username.github.io/Website
+    // assetPrefix: '/Website',
 }
 
 export default nextConfig 
